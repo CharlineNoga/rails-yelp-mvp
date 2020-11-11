0fbf8d7d45bb685c463f386f4bb473a95b61f6ab
charline noga <charlinenoga@gmail.com>
2020-11-11 14:00:43 +0100

    both models added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+les_amis = { name: 'Les amis', address: 'Paris, France', phone_number: 013464772, category: 'french' }
+mai_tai = { name: 'Mai Tai', address: 'Munich, Germany', phone_number: 08975485524, category: 'chinese' }
+bella_ciao = { name: 'Bella Ciao', address: 'Munich, GErmany', phone_number: 08912436657, category: 'italian' }
+sudo_kan = { name: 'Sudo Kan', address: 'Paris, France', phone_number: 0147552614, category: 'japanese' }
+anvers_toi = { name: 'Anvers toi', address: 'Paris, France', phone_number: 0144586312, category: 'belgian' }
+
+
+[ les_amis, mai_tai, ella_ciao, sudo_kan, anvers_toi ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished with #{Restaurant.last}!"
